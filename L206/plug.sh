681f3df82bb92812d7687fda200866d704e527b4
Joey Wang <joeywang@uber.com>
2015-12-27 17:19:48 -0800

    Remove useless comment in plug.sh

--- L206/plug.sh
+++ L206/plug.sh
@@ -1,16 +1,4 @@
 ##!/bin/bash
-#for I in {0..500}; do
-#	echo "Do you want to bind USB device (Y/N)?"
-#	read ans
-#	if [ "$ans" -eq 'Y'] 
-#		echo "1-1" > /sys/bus/usb/drivers/usb/bind
-#	fi
-#	echo "Do you want to unbind USB device (Y/N)?"
-#	read ans
-#	if ["$ans" -eq
-#		echo "1-1" > /sys/bus/usb/drivers/usb/unbind
-#	fi
-#done
 if [ $# -eq 1 ]; then
 	for i in `seq 1 5`;
 	do
